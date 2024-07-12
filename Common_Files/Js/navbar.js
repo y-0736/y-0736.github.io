acd0a8e59215b5f7587a7172102a544c4a820ade
y-0736 <134215612+y-0736@users.noreply.github.com>
2024-07-12 09:06:39 +0300

    Test Update

--- Common_Files/Js/navbar.js
+++ Common_Files/Js/navbar.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/Common_Files/html/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+        });
+});
+
 function toggleSidebar() {
     var sidebar = document.getElementById("mySidebar");
     var navbar = document.getElementById("navbar");
