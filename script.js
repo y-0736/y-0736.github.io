86d4fa6f3eaaee1809950889f1e51ebebbdcd4a3
y-0736 <134215612+y-0736@users.noreply.github.com>
2025-05-10 21:56:24 +0300

    updatr

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ function createFlower() {
     flower.style.animationDuration = (4 + Math.random() * 3) + 's';
     document.body.appendChild(flower);
     setTimeout(() => flower.remove(), 7000);
-  }
-  
-  setInterval(createFlower, 500);
-  
+}
+
+setInterval(createFlower, 500);
+
+function showScreenSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    document.getElementById("screenSize").textContent =
+        `📱 ${width}px x ${height}px`;
+}
+
+window.addEventListener("resize", showScreenSize); // ekran dönünce günceller
+showScreenSize(); // sayfa açılır açılmaz göster
